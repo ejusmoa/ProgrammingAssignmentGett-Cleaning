23aea832a4461068b2d85eafc3a2c8b48dfb06f1
Juan Carlos <juan.carlos.moreno.reina@ericsson.com>
2017-04-23 12:32:22 +0200

    Update Assigment_W4.R
    minor typos fixed.

--- Assigment_W4.R
+++ Assigment_W4.R
@@ -5,7 +5,7 @@ Assigment_W4 <- function() {
         Header_Features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE)
         Header_Features_Vector <- Header_Features[,2]
         
-        # Reads de activities from activity_labels.txt in a dataset and sets the names of the columns
+        # Reads the activities from activity_labels.txt in a dataset and sets the names of the columns
         Activity_Label <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,dec=".")
         colnames(Activity_Label) <- c("id","label")
         
@@ -24,7 +24,7 @@ Assigment_W4 <- function() {
         # Reads the Activities IDs from y_train.txt in a data frame and sets the column name
         Activities_ID <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE,dec=".")
         colnames(Activities_ID) <- c("id")
-        # Joins Activities_ID and Activity_Label by column "id"
+        # Joins Activities_ID and Activity_Label by the variable "id"
         Activities <- plyr::join(Activities_ID,Activity_Label,by="id")
         # Adds the Activities labels as a new column on the left and sets its column name
         X_train_meanStd <- cbind(Activities$label,X_train_meanStd)
@@ -33,7 +33,7 @@ Assigment_W4 <- function() {
         # Reads the subjects IDs from subject_train.txt in a dataframe and sets the column name
         Subjects <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE,dec=".")
         colnames(Subjects) <- c("subject_id")
-        # Adds the subjects IDs as a new column on the left
+        # Adds the subjects' IDs as a new column on the left
         X_train_meanStd <- cbind(Subjects,X_train_meanStd)
         
         ####################
@@ -48,16 +48,16 @@ Assigment_W4 <- function() {
         # Selects only the measurements on the mean and standard deviation for each measurement 
         X_test_meanStd <- select(X_test, matches("\\.mean\\.|\\.std\\."))
         
-        # Reads the Activities IDs from y_train.txt in a data frame and sets the column name
+        # Reads the Activities IDs from y_test.txt in a data frame and sets the column name
         Activities_ID <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE,dec=".")
         colnames(Activities_ID) <- c("id")
-        # Joins Activities_ID and Activity_Label by column "id"
+        # Joins Activities_ID and Activity_Label by the variable "id"
         Activities <- plyr::join(Activities_ID,Activity_Label,by="id")
         # Adds the Activities labels as a new column on the left and sets its column name
         X_test_meanStd <- cbind(Activities$label,X_test_meanStd)
         colnames(X_test_meanStd)[1]="activity"
         
-        # Reads the subjects IDs from subject_test.txt in a dataframe and sets the column name
+        # Reads the subjects' IDs from subject_test.txt in a dataframe and sets the column name
         Subjects <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE,dec=".")
         colnames(Subjects) <- c("subject_id")
         # Adds the subjects IDs as a new column on the left
@@ -71,12 +71,12 @@ Assigment_W4 <- function() {
         test_and_train <- bind_rows(X_test_meanStd,X_train_meanStd)
         # Sorts the dataframe test_and_train by subject_id first and then by activity 
         test_and_train <- arrange(test_and_train,subject_id,activity)
-        # GroupS dataframe test_and_train by subject and activity
+        # Groups dataframe test_and_train by subject and activity
         test_and_train_grouped <- group_by(test_and_train,subject_id,activity)
         # Summarizes all the variables in test_and_train_grouped with their means and casts
         # the result to a dataframe
         Avg_Per_Act_And_Subject <- as.data.frame(dplyr::summarize_all(test_and_train_grouped,mean))
-        # Adds the string "avg_" at the beginning of each of the averages computed in the previous step 
+        # Adds the string "avg_" at the beginning of each of the variable names computed in the previous step 
         add_avg <- function(x){paste("avg_",x,sep="")}
         colnames(Avg_Per_Act_And_Subject)[3:ncol(Avg_Per_Act_And_Subject)] <- sapply(colnames(Avg_Per_Act_And_Subject)[3:ncol(Avg_Per_Act_And_Subject)],add_avg)
         # Replaces the string "..." by "_" and removes the remaining dots in the variables names
@@ -88,6 +88,5 @@ Assigment_W4 <- function() {
         
         # Writes the resulting dataset into myTidyDataset.txt
         write.table(Avg_Per_Act_And_Subject,"./myTidyDataset.txt",row.name=FALSE)
-        
-        
+              
 }
